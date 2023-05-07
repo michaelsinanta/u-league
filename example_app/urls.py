899077d53ee5522017147bc0142f648a0f5435ec
Maulana Bayu Risma Santoso Sari <maulana.bayu@ui.ac.id>
2023-05-07 12:08:40 +0700

    feat:dashboard page  for manajer, panitia, penonton

--- example_app/urls.py
+++ example_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from example_app.views import index
+from example_app.views import index, dashboardManajer, dashboardPenonton, dashboardPanitia
 
 app_name = 'example_app'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('dashboard-manajer', dashboardManajer, name='dashboardManajer'),
+    path('dashboard-penonton', dashboardPenonton, name='dashboardPenonton'),
+    path('dashboard-panitia', dashboardPanitia, name='dashboardPanitia'),
 ]
--- example_app/views.py
+++ example_app/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def dashboardManajer(request):
+    return render(request, 'dashboardManajer.html')
+
+def dashboardPenonton(request):
+    return render(request, 'dashboardPenonton.html')
+
+def dashboardPanitia(request):
+    return render(request, 'dashboardPanitia.html')
