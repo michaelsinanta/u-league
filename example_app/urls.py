80d8371bdba634cc6641e6fca136f8e1166d7505
barrysalim <syafribarry@yahoo.com>
2023-05-07 19:03:14 +0700

    implement login, manage pertandingan, landing page

--- example_app/urls.py
+++ example_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from example_app.views import index, dashboardManajer, dashboardPenonton, dashboardPanitia
+from example_app.views import index, dashboardManajer, dashboardPenonton, dashboardPanitia, login, manage_pertanding, landing_page
 
 app_name = 'example_app'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('login/', index, name='login'),
-    path('manage-pertandingan/', index, name='manage-pertandingan'),
+    path('login/', login, name='login'),
+    path('landing-page/', landing_page, name='landing-page'),
+    path('manage-pertandingan/', manage_pertanding, name='manage-pertandingan'),
     path('dashboard-manajer', dashboardManajer, name='dashboardManajer'),
     path('dashboard-penonton', dashboardPenonton, name='dashboardPenonton'),
     path('dashboard-panitia', dashboardPanitia, name='dashboardPanitia'),
--- example_app/views.py
+++ example_app/views.py
@@ -3,11 +3,15 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+def landing_page(request):
+    return render(request, 'landing_page.html')
+
 def login(request) :
     return render(request, 'login.html')
 
 def manage_pertanding(request) :
-    return render(request, 'management.html')
+    return render(request, 'manage_pertandingan.html')
+
 def dashboardManajer(request):
     return render(request, 'dashboardManajer.html')
 
