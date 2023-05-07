67181e2ed66c02b937208b5ca4934b532f0b84eb
michaelsinanta <michaelchristlambert@gmail.com>
2023-05-07 15:28:56 +0700

    feat: r list pertandingan and history rapat done

--- example_app/urls.py
+++ example_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('pembuatan-pertandingan', pembuatan_pertandingan, name='pembuatan_pertandingan'),
     path('list-waktu-stadium', list_waktu_stadium, name='list_waktu_stadium'),
     path('buat-pertandingan', buat_pertandingan_antar_2tim, name='buat_pertandingan_antar_2tim'),
+    path('list-pertandingan', list_pertandingan, name='list_pertandingan'),
+    path('history-rapat', history_rapat, name='history_rapat'),
 ]
--- example_app/views.py
+++ example_app/views.py
@@ -11,3 +11,9 @@ def list_waktu_stadium(request):
 
 def buat_pertandingan_antar_2tim(request):
     return render(request, 'buat_pertandingan_antar_2tim.html')
+
+def list_pertandingan(request):
+    return render(request, 'list_pertandingan.html')
+
+def history_rapat(request):
+    return render(request, 'history_rapat.html')
