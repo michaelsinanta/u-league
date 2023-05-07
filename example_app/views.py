8025f9cd347b4611f898d1de2b5bb515d35edf94
michaelsinanta <michaelchristlambert@gmail.com>
2023-05-07 13:34:55 +0700

    feat : navbar done

--- example_app/views.py
+++ example_app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def pembuatan_pertandingan(request):
+    return render(request, 'pembuatan_pertandingan.html')
