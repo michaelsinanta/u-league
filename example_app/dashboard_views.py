b9f3b8082357668fc2f77a4ffd7a80ac3a64ee9e
Maulana Bayu Risma Santoso Sari <maulana.bayu@ui.ac.id>
2023-05-24 15:40:52 +0700

    fix dashboard navbar

--- example_app/dashboard_views.py
+++ example_app/dashboard_views.py
@@ -18,13 +18,16 @@ def dashboard(request):
     if role == "Manajer":
         with connection.cursor() as cursor:
             cursor.execute(f'''
-                SELECT N.nama_depan, N.nama_belakang, N.nomor_hp, N.email, N.alamat
-                FROM NON_PEMAIN AS N, MANAJER AS M
-                WHERE M.username = '{username}' AND M.id_manajer = N.id;
+                SELECT N.nama_depan, N.nama_belakang, N.nomor_hp, N.email, N.alamat, S.status
+                FROM NON_PEMAIN AS N, MANAJER AS M, STATUS_NON_PEMAIN AS S
+                WHERE M.username = '{username}' AND M.id_manajer = N.id AND N.id = S.ID_Non_Pemain;
             ''')
             user_list = dict_fetch_all(cursor)
             context = {
-                'user_list': user_list
+                'user_list': user_list,
+                'user': {
+                    'role': f'{role}',
+                }
             }
             return render(request, 'dashboard_manajer.html', context)
     elif role == "Panitia":
